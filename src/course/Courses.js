eafb25bc918d432481b155ea5b4a6a8cca27ac4d
Linlin <linlin@d-i89-172-137.student.eduroam.uq.edu.au>
2020-10-27 23:03:44 +1000

    update

--- src/course/Courses.js
+++ src/course/Courses.js
@@ -1,7 +1,88 @@
 import React from 'react';
-import CourseDetails from './CourseDetails';
+import{ Button, Container, Pagination, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
-const CourseView = props => {
+import CourseCard from './components/CourseCard';
+import ErrorMessage from '../UI/errorMessage/ErrorMessage';
+import FlexContainer from '../UI/flexContainer/FlexContainer';
+import Header from '../UI/header/Header';
+import { COURSE_BASE_URL } from '../routes/URLMap';
+import { fetchCourses } from '../api/course';
+
+
+class Courses extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            courses: [],
+            eroor: null,
+            isLoading: false,
+            pagination: {},
+        };
+    }
+
+    componentDidMount() {
+        this.loadCourses();
+    }
+
+    loadCourses = (pageNum, pageSize) => {
+        this.setState({ isLoading: true, courses: []}, () => {
+            fetchCourses(pageNum, pageSize)
+            .then(this.updateCourseData)
+            .then(error => this.setState({ error }));
+        });
+    }
+
+   updateCourseData = courseData => {
+       this.setState({
+           courses: courseData.courses,
+           isLoading: false,
+           pagination: courseData.pagination,
+       })
+   }
+
+   handlePageChange = (event, data) => {
+       this.loadCourses(data.activePage);
+   }
+
+   render(){
+       const currentPath = this.props.location.pathname;
+
+       return (
+           <React.Fragment>
+               <ErrorMessage error={this.state.error} />
+               <Header as="h2" textAlign="center">
+                   Courses
+               </Header>
+               <Container>
+                   <Button as={Link} to={`${currentPath}/new`} primary>
+                       create new course
+                   </Button>
+                   <segment basic loading={this.state.isLoading}>
+                   <FlexContainer justifyContentValue="space-between">
+                       {this.state.courses.map(course => (
+                           <CourseCard 
+                                courseDescription={course.description}
+                                courseImage={course.image}
+                                courseName={course.name}
+                                key={course.code}
+                                to={`${COURSE_BASE_URL}/${course.code}`}
+                             />
+                       ))}
+                   </FlexContainer>
+                   </segment>
+               </Container>
+           </React.Fragment>
+       )
+   }
+
+
+}
+
+
+/*
+const Course = props => {
     return <div>
        courseView
         
@@ -11,6 +92,7 @@ const CourseView = props => {
         <CourseCard course={course} />
     })}
     */
-};
+// };
 
-export default CourseView;
+
+export default Courses;
